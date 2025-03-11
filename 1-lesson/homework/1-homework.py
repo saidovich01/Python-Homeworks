003dd2e5477519a8af0a49a67cdaff7f540dd66a
Сардор <sardorkalbayev@gmail.com>
2025-03-11 23:10:16 +0500

    Update 1-homework.py

--- 1-lesson/homework/1-homework.py
+++ 1-lesson/homework/1-homework.py
@@ -1,1 +1,29 @@
+# Task-1
+a = int(input("Enter the side of square: "))
+P = 4*a
+S = a**4
 
+print(f"Side {a}\nPerimeter of Square {P}\nArea of Square {S}")
+
+# Task-2
+D = int(input("Enter the diameter of circle: "))
+L = 3.14*D
+
+print(f"Diameter {D}\nLength of Circle {L}")
+
+# Task-3
+a = int(input("Enter the value: "))
+b = int(input("Enter the value: "))
+AVG = (a+b)/2
+
+print(f"First Value {a}\nSecond Value {b}\nAverage {AVG}")
+
+# Task-4
+a = int(input("Enter the value: "))
+b = int(input("Enter the value: "))
+SUM = a+b
+PRODUCT = a*b
+A2 = a**2
+B2 = b**2
+
+print(f"First Value {a}\nSecond Value {b}\nSum {SUM}\nProduct {PRODUCT}\na^2 {A2}\nb^2 {B2}")
